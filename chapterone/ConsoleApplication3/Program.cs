5ed241927aef01a2c9ea0ae0b3f18f026e855496
RMiller28 <Millerrycki5@gmail.com>
2014-02-12 10:25:59 -0500

    finish

---
+++ chapterone/ConsoleApplication3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  x ");
+            Console.WriteLine(" xxx ");
+            Console.WriteLine(" xxxx ");
+            Console.WriteLine("xxxxxx");
+            Console.WriteLine("  x ");
+        }
+    }
+}
---
+++ chapterone/Inititals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inititals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("rrrrr   mmm mmm mmm mmm");
+            Console.WriteLine("rr   rr mmm   mmm   mmm");
+            Console.WriteLine("rr   rr mmm   mmm   mmm");
+            Console.WriteLine("rr  rr  mmm         mmm");
+            Console.WriteLine("rrrr    mmm         mmm");
+            Console.WriteLine("rr  rr  mmm         mmm");
+            Console.WriteLine("rr   rr mmm         mmm");
+            Console.WriteLine("rr   rr mmm         mmm");
+
+        }
+
+    }
+}
